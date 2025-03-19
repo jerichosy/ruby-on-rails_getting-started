4ace58aca2a11a4af05105ee3c4e467615b4fc54
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2025-03-19 10:59:43 +0800

    done Create (and R) of CRUD

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
 
-# ? Why is this empty
+# * Active Record automatically defines model attributes for every table column, so you don't have to declare those attributes in your model file.
